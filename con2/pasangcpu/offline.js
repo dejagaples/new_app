bacef5b341d07b762dcb7b182098de5f401055da
dejagaples <febrymahfuz@gmail.com>
2020-11-24 16:37:59 +0800

    add_con2

---
+++ con2/pasangcpu/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1605310182,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"images/tiledbackground2.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/sprite3-sheet0.png",
+		"images/tiledbackground4.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"cpu_1.mp4",
+		"cpu_2.mp4"
+	]
+}
---
+++ con2/pasangodd/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1605594188,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"odd2.mp4"
+	]
+}
